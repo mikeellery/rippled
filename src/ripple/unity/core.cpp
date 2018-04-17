5242b1be933b5e719cf135eea0e4f5f4ce935434
Mike Ellery <mellery451@gmail.com>
2018-04-17 12:29:22 -0700

    NIH deps for libs.
    EP for soci. sqlite as lib. EP for snappy.
    lz4 as lib. secp256 and ed25519 as libs.
    EP for rocksdb. New EP for libarchive.

--- src/ripple/unity/core.cpp
+++ src/ripple/unity/core.cpp
@@ -20,11 +20,13 @@
 
 #include <ripple/core/impl/Config.cpp>
 #include <ripple/core/impl/DatabaseCon.cpp>
+#include <ripple/core/impl/DummySociDynamicBackend.cpp>
 #include <ripple/core/impl/LoadEvent.cpp>
 #include <ripple/core/impl/LoadMonitor.cpp>
 #include <ripple/core/impl/Job.cpp>
 #include <ripple/core/impl/JobQueue.cpp>
 #include <ripple/core/impl/SNTPClock.cpp>
+#include <ripple/core/impl/SociDB.cpp>
 #include <ripple/core/impl/Stoppable.cpp>
 #include <ripple/core/impl/TerminateHandler.cpp>
 #include <ripple/core/impl/TimeKeeper.cpp>
